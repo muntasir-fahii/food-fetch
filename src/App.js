f6fc9fa699ed37294eebd35e5d2cb4ac9d0f96af
muntasir-fahii <fa3877957@gmail.com>
2023-10-12 20:23:19 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,65 +1,7 @@
-import { useEffect, useState } from "react";
-import Bookmark from "./components/Bookmark";
-import Card from "./components/Card";
+import TodoApp from "./components/TodoApp";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [meals, setMeals] = useState([]);
-  const [bookmark, setBookmark] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-      .then((res) => res.json())
-      .then((data) => setMeals(data?.meals));
-  }, [search]);
-
-  const bookmarkHandler = (meal) => {
-    if (bookmark) {
-      const isExistData = bookmark.find((data) => data.idMeal === meal.idMeal);
-      if (isExistData) {
-        alert("already exist");
-      } else {
-        setBookmark([...bookmark, meal]);
-      }
-    } else {
-      setBookmark([meal]);
-    }
-  };
-
-  const deleteHandler = (item) => {
-    const updatedData = bookmark.filter((data) => data.idMeal !== item.idMeal);
-    if (updatedData) {
-      setBookmark(updatedData);
-    }
-  };
-
-  return (
-    <div className="app">
-      <div className="input">
-        <input
-          type="text"
-          value={search}
-          placeholder="Search our menu here"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
-      <div className="content">
-        <div className="left">
-          {meals?.map((meal) => (
-            <Card
-              key={meal.idMeal}
-              meal={meal}
-              bookmarkHandler={bookmarkHandler}
-            />
-          ))}
-        </div>
-
-        <div className="right">
-          <Bookmark bookmark={bookmark} deleteHandler={deleteHandler} />
-        </div>
-      </div>
-    </div>
-  );
+  return <TodoApp />;
 }
 
 export default App;
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() === "") {
+      return;
+    }
+
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { text: newTask, completed: false },
+    ]);
+    setNewTask("");
+  };
+
+  const handleCompleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <>
+      <h2 className="headline">ToDo List</h2>
+
+      <div className="to-do">
+        <div className="task-form">
+          <input
+            type="text"
+            placeholder="Add a new task"
+            value={newTask}
+            onChange={handleInputChange}
+            className="input"
+          />
+          <button onClick={handleAddTask} className="add-btn">
+            Add
+          </button>
+        </div>
+        <div className="tasks">
+          <div className="task">
+            {tasks.map((task, index) => (
+              <div key={index} className="task-item">
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => handleCompleteTask(index)}
+                  className="input"
+                />
+                <span
+                  style={{
+                    textDecoration: task.completed ? "line-through" : "none",
+                  }}
+                >
+                  {task.text}
+                </span>
+                <button
+                  onClick={() => handleDeleteTask(index)}
+                  className="delete-btn"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TodoApp;
